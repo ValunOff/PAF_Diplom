6ed9965ecf03be634ed14b30c6b03a5d1d980502
Timoha <tim.valiev.00@mail.ru>
2021-05-09 22:35:15 +0500

    я тут что то в сонном состоянии накодил, вроде работает

--- PAF/ViewModel/ClientVM.cs
+++ PAF/ViewModel/ClientVM.cs
@@ -1,6 +1,7 @@
 ﻿using PAF.Commands.Base;
 using PAF.Data;
 using PAF.Data.Entityies;
+using PAF.View.Windows;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
@@ -9,8 +10,12 @@ namespace PAF.ViewModel
 {
     class ClientVM : ViewModel
     {
-        /// <summary>Разрешение нажатия для всех управляемых кнопок таблицы пока изменения не будут занесены в бд</summary>
-        bool CanButtonClick = true;        
+        /// <summary>Пока прога работает с бд, лучше запретить все кнопки для работы с бд</summary>
+        bool CanButtonClick = true;
+
+        Clients _AddClient = new Clients();
+        /// <summary>Данные нового клиента</summary>
+        public Clients AddClient { get => _AddClient; set => Set(ref _AddClient, value); }
 
         #region Commands
 
@@ -32,7 +37,9 @@ namespace PAF.ViewModel
         private void OnAddExecuted(object p)
         {
             CanButtonClick = false;
-            
+            //AddClient = new Clients();
+            ClientAdd clientAdd = new ClientAdd();
+            clientAdd.ShowDialog();
             CanButtonClick = true;
         }
         #endregion
@@ -58,6 +65,17 @@ namespace PAF.ViewModel
             CanButtonClick = true;
         }
         #endregion
+
+        #region AddClientCommand
+        public ICommand AddClientCommand { get; set; }
+        private bool CanAddClientExecute(object p) => CanButtonClick;
+        private void OnAddClientExecuted(object p)
+        {
+            CanButtonClick = false;
+            //MessageBox.Show(AddClient.FirstName + " " + AddClient.LastName + " " + AddClient.MiddleName);
+            CanButtonClick = true;
+        }
+        #endregion
         #endregion
 
         public List<Clients> Clients { get => _Clients; set => Set(ref _Clients, value); }
@@ -69,6 +87,8 @@ namespace PAF.ViewModel
             SaveChangesCommand = new LambdaCommand(OnSaveChangesExecuted, CanSaveChangesExecute);
             AddCommand = new LambdaCommand(OnAddExecuted, CanAddExecute);
             UpdateCommand = new LambdaCommand(OnUpdateExecuted, CanUpdateExecute);
+            DeleteCommand = new LambdaCommand(OnDeleteExecuted, CanDeleteExecute);
+            AddClientCommand = new LambdaCommand(OnAddClientExecuted, CanAddClientExecute);
             #endregion
         }
     }
