b63b64cf3b9f1b1d3f125fc01e4857132a177f5e
Timoha <tim.valiev.00@mail.ru>
2021-05-16 19:45:25 +0300

    перекинул дата контекст в код после инициализации

--- PAF/View/Pages/Client.xaml.cs
+++ PAF/View/Pages/Client.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PAF.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace PAF.View.Pages
         public Client()
         {
             InitializeComponent();
+            this.DataContext = new ClientVM();
         }
     }
 }
