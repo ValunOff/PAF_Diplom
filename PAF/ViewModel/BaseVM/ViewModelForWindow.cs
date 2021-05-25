40c436dee099abced2dc84bd3e8c2e9a8ba5ef31
Timoha <tim.valiev.00@mail.ru>
2021-05-25 16:55:59 +0500

    сделал этот класс паблик

--- PAF/ViewModel/BaseVM/ViewModelForWindow.cs
+++ PAF/ViewModel/BaseVM/ViewModelForWindow.cs
@@ -6,7 +6,7 @@ using System.Windows.Input;
 
 namespace PAF.ViewModel
 {
-    class ViewModelForWindow : INotifyPropertyChanged
+    public class ViewModelForWindow : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
