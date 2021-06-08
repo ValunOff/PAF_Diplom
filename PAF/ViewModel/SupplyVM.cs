c114bf12eaed3ac685612aaf87227dceea602fe0
Timoha <tim.valiev.00@mail.ru>
2021-06-08 10:34:21 +0500

    относится к предыдущему коммиту

--- PAF/ViewModel/SupplyVM.cs
+++ PAF/ViewModel/SupplyVM.cs
@@ -1,10 +1,7 @@
 ﻿using PAF.Commands.Base;
-using PAF.Data;
-using PAF.Data.Entityies;
 using PAF.View.Windows;
 using PAF.ViewModel.BaseVM;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
